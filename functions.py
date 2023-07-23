6520373ee20a2f64420dbbc3c2dd8437f4b8ac27
jsaj <j.jr.avelino@gmail.com>
2023-07-23 15:11:48 -0300

    Initial commit

---
+++ functions.py
@@ -0,0 +1,258 @@
+import pandas as pd
+
+from import_packages import *
+
+# Função para remover acentos usando a biblioteca unidecode
+def remove_accent(text):
+    return unidecode(text)
+
+# Função para remover caracteres especiais usando expressões regulares
+def remove_special_characters(text):
+    return re.sub(r'[^a-zA-Z0-9\s]', '', text)
+
+# Função para simplificar o nome em caso de repetição de letras
+def simplify_name(name):
+    for i in range(1, len(name)):
+        if name[i] == name[i - 1]:
+            return name[:i]
+    return name
+
+# Função que aplica a simplificação apenas para as linhas com "repeat_count" maior que 1
+def replace_value_by_id(df, id_col, id_name):
+    return df.groupby(id_col)[id_name].transform(lambda x: simplify_name(x.iloc[0]) if x.iloc[0].count(r"(.)\1+") > 1 else x.iloc[0])
+
+def read_receita():
+    meses = [
+        "JANEIRO",
+        "FEVEREIRO",
+        "MARCO",
+        "ABRIL",
+        "MAIO",
+        "JUNHO",
+        "JULHO",
+        "AGOSTO",
+        "SETEMBRO",
+        "OUTUBRO",
+        "NOVEMBRO",
+        "DEZEMBRO"
+    ]
+
+    anos = [2021, 2022, 2023]
+
+    df_receita = []
+
+    for ano in anos:
+        count = 1
+        for mes in meses:
+            if ano == 2023 and mes not in ["AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"]:
+                file_path = 'C:\\Users\\Jjr_a\\OneDrive\\Documentos\\Estudo\\Forecasting\\Despesa_receita\\RECEITA_ATE_{}_{}.csv'.format(
+                    mes, ano)
+                df = pd.read_csv(file_path, encoding='ISO-8859-1', delimiter=';')
+
+                if count < 10:
+                    df['ds'] = '{}-0{}-01'.format(ano, count)
+                else:
+                    df['ds'] = '{}-{}-01'.format(ano, count)
+            elif ano != 2023:
+                file_path = 'C:\\Users\\Jjr_a\\OneDrive\\Documentos\\Estudo\\Forecasting\\Despesa_receita\\RECEITA_ATE_{}_{}.csv'.format(
+                    mes, ano)
+                df = pd.read_csv(file_path, encoding='ISO-8859-1', delimiter=';')
+
+                if count < 10:
+                    df['ds'] = '{}-0{}-01'.format(ano, count)
+                else:
+                    df['ds'] = '{}-{}-01'.format(ano, count)
+            # Criar o índice de datas com todos os dias do mês específico
+
+            df['ds'] = pd.to_datetime(df['ds'])
+            if df['Vlr. Receita Prevista'].dtypes == object:
+                df['Vlr. Receita Prevista'] = df['Vlr. Receita Prevista'].str.replace(',', '.').astype(float)
+            if df['Vlr. Receita Realizada'].dtypes == object:
+                df['Vlr. Receita Realizada'] = df['Vlr. Receita Realizada'].str.replace(',', '.').astype(float)
+            if df['% Realizado'].dtypes == object:
+                df['% Realizado'] = df['% Realizado'].str.replace(',', '.').astype(float)
+
+            df_receita.append(df)
+            count += 1
+        count = 0
+    df_receita = pd.concat(df_receita)
+    df_receita = df_receita.loc[df_receita['ds'] <= '2023-08-01']
+    df_receita = df_receita.sort_values(by=['ds', 'Cód. Unidade Gestora'])
+    df_receita = df_receita.drop_duplicates().reset_index(drop=True)
+    for column in df_receita.columns:
+        if df_receita[column].dtypes == object:
+            df_receita[column] = df_receita[column].apply(remove_accent)
+            df_receita[column] = df_receita[column].str.upper()
+            df_receita[column] = df_receita[column].str.strip()
+
+    return df_receita
+
+def read_despesa():
+    meses = [
+        "JANEIRO",
+        "FEVEREIRO",
+        "MARCO",
+        "ABRIL",
+        "MAIO",
+        "JUNHO",
+        "JULHO",
+        "AGOSTO",
+        "SETEMBRO",
+        "OUTUBRO",
+        "NOVEMBRO",
+        "DEZEMBRO"
+    ]
+
+    anos = [2021, 2022, 2023]
+
+    df_despesa = []
+
+
+    for ano in anos:
+        count = 1
+        for mes in meses:
+            if ano == 2023 and mes not in ["AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"]:
+
+                file_path = 'C:\\Users\\Jjr_a\\OneDrive\\Documentos\\Estudo\\Forecasting\\Despesa_receita\\DESPESA_PAGA_ATE_{}_{}.csv'.format(
+                    mes, ano)
+                df = pd.read_csv(file_path, encoding='ISO-8859-1', delimiter=';')
+
+                if count < 10:
+                    df['ds'] = '{}-0{}-01'.format(ano, count)
+                else:
+                    df['ds'] = '{}-{}-01'.format(ano, count)
+            elif ano != 2023:
+                file_path = 'C:\\Users\\Jjr_a\\OneDrive\\Documentos\\Estudo\\Forecasting\\Despesa_receita\\DESPESA_PAGA_ATE_{}_{}.csv'.format(
+                    mes, ano)
+                df = pd.read_csv(file_path, encoding='ISO-8859-1', delimiter=';')
+
+                if count < 10:
+                    df['ds'] = '{}-0{}-01'.format(ano, count)
+                else:
+                    df['ds'] = '{}-{}-01'.format(ano, count)
+            # Criar o índice de datas com todos os dias do mês específico
+
+            df['ds'] = pd.to_datetime(df['ds'])
+            if df['Valor Empenhado'].dtypes == object:
+                df['Valor Empenhado'] = df['Valor Empenhado'].str.replace(',', '.').astype(float)
+            if df['Valor Liquidado'].dtypes == object:
+                df['Valor Liquidado'] = df['Valor Liquidado'].str.replace(',', '.').astype(float)
+            if df['Valor Pago'].dtypes == object:
+                df['Valor Pago'] = df['Valor Pago'].str.replace(',', '.').astype(float)
+
+            df_despesa.append(df)
+            count += 1
+        count = 0
+    df_despesa = pd.concat(df_despesa)
+    df_despesa = df_despesa.loc[df_despesa['ds'] <= '2023-08-01']
+
+    df_despesa = df_despesa.rename(columns={'Órgão': 'Unidade Gestora'}).groupby(['Unidade Gestora', 'ds']).agg({'Valor Empenhado': 'sum',
+                                                                                                                 'Valor Liquidado': 'sum',
+                                                                                                                 'Valor Pago': 'sum'}).reset_index()
+    df_despesa = df_despesa.sort_values(by=['ds', 'Unidade Gestora'])
+    df_despesa = df_despesa.drop_duplicates().reset_index(drop=True)
+
+    for column in df_despesa.columns:
+        if df_despesa[column].dtypes == object:
+            df_despesa[column] = df_despesa[column].apply(remove_accent)
+            df_despesa[column] = df_despesa[column].str.upper()
+            df_despesa[column] = df_despesa[column].str.strip()
+    return df_despesa
+
+
+def get_date_range(start_date, end_date):
+    start_date = datetime.strptime(start_date, '%Y-%m-%d').replace(day=1)
+    end_date = datetime.strptime(end_date, '%Y-%m-%d').replace(day=1)
+
+    current_date = start_date
+    dates_list = []
+
+    while current_date <= end_date:
+        dates_list.append(current_date.strftime('%Y-%m-%d'))
+        current_date += relativedelta(months=1)
+
+    return dates_list
+
+def create_test_data(train, n_periods, exog_var):
+    start_date = datetime.now().replace(day=1) + relativedelta(months=1)
+    end_date = start_date + relativedelta(months=n_periods-1)
+
+    start_date = start_date.strftime('%Y-%m-%d')
+    end_date = end_date.strftime('%Y-%m-%d')
+
+    test = pd.DataFrame(get_date_range(start_date, end_date), columns=['ds'])
+    test['ds'] = pd.to_datetime(test['ds'])
+
+    # Adicionando a coluna 'y' em test com os valores médios dos últimos anos
+    test[exog_var] = test['ds'].apply(lambda date: train[train['ds'].dt.year == date.year - 1][exog_var].mean())
+
+    return test
+
+def predict_ARIMA(df, n_periods, exog_var=None):
+
+    # # Obter o mês atual em formato de número (de 1 a 12)
+
+    # Separação dos dados em conjuntos de treinamento e teste
+    # train = df.loc[df['ds'] < f'2023-0{mes_atual - n_periods}-01']
+    # test = df.loc[df['ds'] >= f'2023-0{mes_atual - n_periods}-01']
+    train = df
+    test = create_test_data(train, n_periods, exog_var)
+    # Obter a data atual no formato 'ano-mes-dia', onde o dia é 01
+
+    # Configura a coluna "ds" como índice do conjunto de treinamento
+    train.set_index('ds', inplace=True)
+    test.set_index('ds', inplace=True)
+
+    if exog_var is not None:
+        # Treinamento do modelo SARIMAX com a variável exógena
+        model = SARIMAX(train["y"], order=(5, 1, 1), exog=train[exog_var])
+    else:
+        model = SARIMAX(train["y"], order=(5, 1, 1))
+
+    model_fit = model.fit()
+
+    # Previsão para os próximos dois períodos (2 valores no total) com a variável exógena
+    if exog_var is not None:
+        forecast = model_fit.get_forecast(steps=n_periods, exog=test[exog_var])
+    else:
+        forecast = model_fit.get_forecast(steps=n_periods)
+
+    predicted_values = forecast.predicted_mean
+    predictions = []
+    for i, date in enumerate(predicted_values.index):
+
+        predictions.append([date, predicted_values.iloc[i]])
+
+    predictions = pd.DataFrame(predictions, columns=['ds', 'yhat'])
+    # predictions = test.merge(predictions, how='left', on=['ds'])
+    # print(train.reset_index().rename(columns={'y': 'yhat'})[predictions.columns].tail(1))
+    predictions = pd.concat([train.reset_index().rename(columns={'y': 'yhat'})[predictions.columns].tail(1), predictions])
+    return predictions
+
+def predict_prophet(df, n_periods, exog_var='Não'):
+    # Criar o modelo de previsão
+    model = Prophet()
+
+    if exog_var != None:
+        # Adicionar a variável exógena ao modelo
+        model.add_regressor(exog_var)
+
+    # Treinar o modelo
+    train = df
+    test = create_test_data(train, n_periods, exog_var)
+
+    model.fit(train)
+
+    # Criar dataframe com as datas futuras para prever os próximos 2 meses
+    # futuro = modelo.make_future_dataframe(periods=n_periods, freq='M')
+    # print(futuro)
+    # if exog_var != None:
+        # futuro = futuro.merge(test[['ds', exog_var]], how='left', on='ds').fillna(0)
+
+    predictions = model.predict(test)
+
+
+    predictions = predictions[['ds', 'yhat']]
+    predictions = pd.concat(
+        [train.rename(columns={'y': 'yhat'})[predictions.columns].tail(1), predictions])
+    return predictions
---
+++ st_forecasting.py
@@ -0,0 +1,241 @@
+from import_packages import *
+from functions import *
+
+st.set_page_config(
+    page_title="Time series annotations", page_icon="⬇"
+)
+
+# @st.cache(allow_output_mutation=True)
+# @st.cache_data
+def load_data():
+    df_despesa = read_despesa()
+    df_receita = read_receita()
+    return df_despesa, df_receita
+
+    # Carrega os dados ou utiliza os dados em cache
+df_despesa, df_receita = load_data()
+
+df_receita_despesa = df_receita.merge(df_despesa, how='left', on=['ds', 'Unidade Gestora']).fillna(0)
+
+
+# Lista com todas as opções únicas da coluna 'Cód. Unidade Gestora'
+opcoes_unidade_gestora = list(df_receita_despesa['Unidade Gestora'].drop_duplicates())
+
+# Sidebar com o filtro
+filtro_unidade_gestora = st.sidebar.selectbox('Unidade Gestora: ',
+                                              opcoes_unidade_gestora,
+                                              index=opcoes_unidade_gestora.index('FUNDO MUNICIPAL DE SAUDE'))
+
+# Sidebar com o filtro dos meses de previsão
+n_periods = st.sidebar.selectbox('Meses de previsão: ', list(range(1, 13)))
+
+# Criar uma barra deslizante (slider) para selecionar a variável exógerna
+model_name = st.sidebar.selectbox('Modelo preditivo:', ['ARIMA', 'Prophet'])
+
+# Filtrar o DataFrame com base nas opções selecionadas no filtro
+df_filtrado = df_receita_despesa[df_receita_despesa['Unidade Gestora'] == filtro_unidade_gestora]
+
+# Criar uma barra deslizante (slider) para selecionar a variável exógerna
+op_exog = st.sidebar.selectbox('Usar variável exógena?:', ['Sim', 'Não'])
+
+if op_exog == 'Sim':
+
+    # Criar uma barra deslizante (slider) para selecionar a variável exógerna
+    exog_var = st.sidebar.selectbox('Variável exógena:', ['Valor Empenhado', 'Valor Liquidado', 'Valor Pago'])
+
+    # Criar uma barra deslizante (slider) para selecionar a porcentagem
+    porcentagem = st.sidebar.slider('% vs. Var. Exógena:', min_value=0, max_value=100, value=100, step=1)
+
+
+    # Renomear as colunas para que o Prophet possa reconhecê-las
+    df_filtrado = df_filtrado.rename(columns={'Vlr. Receita Realizada': 'y'})
+
+    # Aplicar a função ao DataFrame para criar uma nova coluna com os valores multiplicados
+    df_filtrado[exog_var] = df_filtrado[exog_var] * (porcentagem / 100)
+
+    # Criar o modelo de previsão
+    if model_name == 'ARIMA':
+        predictions = predict_ARIMA(df=df_filtrado, n_periods=n_periods, exog_var=exog_var)
+        df_filtrado = df_filtrado.reset_index()
+    elif model_name == 'Prophet':
+        predictions = predict_prophet(df=df_filtrado, n_periods=n_periods, exog_var=exog_var)
+
+
+else:
+    # Criar o modelo de previsão
+    if model_name == 'ARIMA':
+        predictions = predict_ARIMA(df=df_filtrado, n_periods=n_periods, exog_var=None)
+        df_filtrado = df_filtrado.reset_index()
+    elif model_name == 'Prophet':
+        predictions = predict_prophet(df=df_filtrado, n_periods=n_periods, exog_var=None)
+
+
+st.write(df_filtrado[['ds', 'y']].tail())
+st.write(predictions)
+#
+# # Criar o gráfico de linhas
+fig = go.Figure()
+
+# Adicionar a série original de receita realizada
+fig.add_trace(go.Scatter(x=df_filtrado['ds'], y=df_filtrado['y'], mode='lines', name='Receita Realizada', line=dict(color='blue')))
+
+# Adicionar a série de previsão de receita
+fig.add_trace(go.Scatter(x=predictions['ds'], y=predictions['yhat'], mode='lines', name='Receita Predita', line=dict(color='green')))
+
+# Personalizar layout do gráfico
+fig.update_layout(title='Receita Realizada vs. Receita Predita',
+                  xaxis_title='Data',
+                  yaxis_title='Receita',
+                  showlegend=True)
+
+# Exibir o gráfico usando Streamlit
+st.plotly_chart(fig)
+
+
+
+
+# m = Prophet()
+#
+# future = m.make_future_dataframe(periods=periods_input)
+
+# @st.experimental_memo(ttl=60 * 60 * 24)
+# def get_chart(data):
+#     hover = alt.selection_single(
+#         fields=["date"],
+#         nearest=True,
+#         on="mouseover",
+#         empty="none",
+#     )
+#
+#     lines = (
+#         alt.Chart(data, height=500, title="Evolution of stock prices")
+#         .mark_line()
+#         .encode(
+#             x=alt.X("date", title="Date"),
+#             y=alt.Y("price", title="Price"),
+#             color="symbol",
+#         )
+#     )
+#
+#     # Draw points on the line, and highlight based on selection
+#     points = lines.transform_filter(hover).mark_circle(size=65)
+#
+#     # Draw a rule at the location of the selection
+#     tooltips = (
+#         alt.Chart(data)
+#         .mark_rule()
+#         .encode(
+#             x="yearmonthdate(date)",
+#             y="price",
+#             opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+#             tooltip=[
+#                 alt.Tooltip("date", title="Date"),
+#                 alt.Tooltip("price", title="Price (USD)"),
+#             ],
+#         )
+#         .add_selection(hover)
+#     )
+#
+#     return (lines + points + tooltips).interactive()
+#
+#
+# st.title("⬇ Time series annotations")
+#
+# st.write("Give more context to your time series using annotations!")
+#
+# col1, col2, col3 = st.columns(3)
+# with col1:
+#     ticker = st.text_input("Choose a ticker (⬇💬👇ℹ️ ...)", value="⬇")
+# with col2:
+#     ticker_dx = st.slider(
+#         "Horizontal offset", min_value=-30, max_value=30, step=1, value=0
+#     )
+# with col3:
+#     ticker_dy = st.slider(
+#         "Vertical offset", min_value=-30, max_value=30, step=1, value=-10
+#     )
+#
+# # Original time series chart. Omitted `get_chart` for clarity
+# source = get_data()
+# chart = get_chart(source)
+#
+# # Input annotations
+# ANNOTATIONS = [
+#     ("Mar 01, 2008", "Pretty good day for GOOG"),
+#     ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
+#     ("Nov 01, 2008", "Market starts again thanks to..."),
+#     ("Dec 01, 2009", "Small crash for GOOG after..."),
+# ]
+#
+# # Create a chart with annotations
+# annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
+# annotations_df.date = pd.to_datetime(annotations_df.date)
+# annotations_df["y"] = 0
+# annotation_layer = (
+#     alt.Chart(annotations_df)
+#     .mark_text(size=15, text=ticker, dx=ticker_dx, dy=ticker_dy, align="center")
+#     .encode(
+#         x="date:T",
+#         y=alt.Y("y:Q"),
+#         tooltip=["event"],
+#     )
+#     .interactive()
+# )
+#
+# # Display both charts together
+# st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
+#
+# st.write("## Code")
+#
+# st.write(
+#     "See more in our public [GitHub"
+#     " repository](https://github.com/streamlit/example-app-time-series-annotation)"
+# )
+#
+# st.code(
+#     f"""
+# import altair as alt
+# import pandas as pd
+# import streamlit as st
+# from vega_datasets import data
+#
+# @st.experimental_memo
+# def get_data():
+#     source = data.stocks()
+#     source = source[source.date.gt("2004-01-01")]
+#     return source
+#
+# source = get_data()
+#
+# # Original time series chart. Omitted `get_chart` for clarity
+# chart = get_chart(source)
+#
+# # Input annotations
+# ANNOTATIONS = [
+#     ("Mar 01, 2008", "Pretty good day for GOOG"),
+#     ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
+#     ("Nov 01, 2008", "Market starts again thanks to..."),
+#     ("Dec 01, 2009", "Small crash for GOOG after..."),
+# ]
+#
+# # Create a chart with annotations
+# annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
+# annotations_df.date = pd.to_datetime(annotations_df.date)
+# annotations_df["y"] = 0
+# annotation_layer = (
+#     alt.Chart(annotations_df)
+#     .mark_text(size=15, text="{ticker}", dx={ticker_dx}, dy={ticker_dy}, align="center")
+#     .encode(
+#         x="date:T",
+#         y=alt.Y("y:Q"),
+#         tooltip=["event"],
+#     )
+#     .interactive()
+# )
+#
+# # Display both charts together
+# st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
+#
+# """,
+#     "python",
+# )
